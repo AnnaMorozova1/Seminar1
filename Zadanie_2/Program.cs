d2ed5cfc7c0572d7fcc28dac290c024c1f8ebee8
Anna <anyutka901990@mail.ru>
2022-08-05 13:51:11 +0300

    Zadanie 2

---
+++ Zadanie_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите числа: ");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+
+if (A > B)
+{
+    Console.WriteLine("max " + A);
+}
+else if (A < B)
+{
+    Console.WriteLine("max " + B);
+}
+else
+{
+    Console.WriteLine(A + " = " + B);
+}
